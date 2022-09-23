6a3e426e56843db4d57781a30431c8ecdcf44df4
Amirouche <bessaa.amirouche@outlook.fr>
2022-09-23 23:13:12 +0200

    premier jour

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-document.querySelector('body').style.background = "red";
+const menuHamburger = document.querySelector(".menu-hamburger");
+const nav = document.querySelector(".nav");
+
+menuHamburger.addEventListener("click", () => {
+  nav.classList.toggle("mobile-menu");
+});
